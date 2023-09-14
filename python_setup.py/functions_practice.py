fd3a33cf9eb1d1e13b793cbf0475be19c9b99d0c
Nalinaka <nalinaka@gmail.com>
2023-09-14 11:41:19 -0700

    Completed assignment

---
+++ python_setup.py/functions_practice.py
@@ -0,0 +1,22 @@
+def hello():
+    print("Hello Human")
+
+
+def pack():
+    list_of_args = pack(1, 2, 3) 
+    return [1, 2, 3]  
+
+
+def eat_lunch():
+    eat_lunch = True
+    if eat_lunch:
+        print("First I eat a sandwich")
+    if eat_lunch:
+        print("Next I eat a cake")
+    else:
+        print("My lunchbox is empty")
+
+
+
+
+
